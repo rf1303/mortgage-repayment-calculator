2522b4939cb826937decef3c70b6d1abf1924e60
Ramiro Fernandez <pulgaramiro@gmail.com>
2026-02-20 11:02:38 -0300

    en estados activos y responsive tablet desktop 04

--- src/form/Error-field.jsx
+++ src/form/Error-field.jsx
@@ -3,6 +3,6 @@
 export const ErrorField = (e) => {
         console.log('e:', e)
     return (
-        <span className='error text-red text-preset-4' id="error-rate" aria-hidden="true">error field</span>
+        <span className='error text-red text-preset-4' id="error-rate" aria-hidden="true">This field is required</span>
     );
 }
